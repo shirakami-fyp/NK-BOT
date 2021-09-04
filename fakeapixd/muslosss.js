abc07c8407934bcbc53e930fa142ce2a22aad1ee
Shirakami <90049408+shirakami-fyp@users.noreply.github.com>
2021-09-04 09:22:24 -0400

    Update muslosss.js

--- fakeapixd/muslosss.js
+++ fakeapixd/muslosss.js
@@ -1,5 +1,8 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvMRPeHtlg3wipWxCS055lBMcQpYn2Pogdyw&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZwjGF4urnf7AI5zVNIaZvbGHtt3u--CzBDw&usqp=CAU"}, 
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2klYuHkNb-PCTvZvuHTS86n5wHWEPutlu1w&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2klYuHkNb-PCTvZvuHTS86n5wHWEPutlu1w&usqp=CAU"}, 
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQuqGqPcPIEVw1B3nuAegQpkfLKDhncTV7ZA&usqp=CAU"}, 
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpXdwK6P2cDmGr2NJTTBvxakqeK0ldks0zxw&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjdraSf2ZrP3OCbdSGy8kXtb5uaZEVa_WnJQ&usqp=CAU"}
 ]
