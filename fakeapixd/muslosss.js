4a6968896df69d3b641c42b22d3962aa4beb7cc4
Shirakami <90049408+shirakami-fyp@users.noreply.github.com>
2021-09-04 03:29:09 -0400

    Update muslosss.js

--- fakeapixd/muslosss.js
+++ fakeapixd/muslosss.js
@@ -1,4 +1,5 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvMRPeHtlg3wipWxCS055lBMcQpYn2Pogdyw&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZwjGF4urnf7AI5zVNIaZvbGHtt3u--CzBDw&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZwjGF4urnf7AI5zVNIaZvbGHtt3u--CzBDw&usqp=CAU"}, 
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2klYuHkNb-PCTvZvuHTS86n5wHWEPutlu1w&usqp=CAU"}
 ]
