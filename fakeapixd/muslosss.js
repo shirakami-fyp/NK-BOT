0d458c43712268904555168c8387db3814c12558
Shirakami <90049408+shirakami-fyp@users.noreply.github.com>
2021-09-06 17:54:50 -0400

    Update muslosss.js

--- fakeapixd/muslosss.js
+++ fakeapixd/muslosss.js
@@ -4,5 +4,6 @@
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2klYuHkNb-PCTvZvuHTS86n5wHWEPutlu1w&usqp=CAU"}, 
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQuqGqPcPIEVw1B3nuAegQpkfLKDhncTV7ZA&usqp=CAU"}, 
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpXdwK6P2cDmGr2NJTTBvxakqeK0ldks0zxw&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjdraSf2ZrP3OCbdSGy8kXtb5uaZEVa_WnJQ&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjdraSf2ZrP3OCbdSGy8kXtb5uaZEVa_WnJQ&usqp=CAU"}, 
+{"result": "http://pm1.narvii.com/7402/d95454e184c022138f748d12360ceab7d07116a5r1-768-960v2_uhq.jpg"}
 ]
